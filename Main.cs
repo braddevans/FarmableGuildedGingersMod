5657a9f0e2b6a0dbf6f69a00c360ead451c21710
bradd evans <braddevans123@gmail.com>
2020-12-24 02:48:20 +0000

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,20 @@
+﻿using SRML;
+
+namespace FarmableGuildedGingersMod {
+    
+    public class Main : ModEntryPoint {
+        public override void PreLoad() {
+            HarmonyInstance.PatchAll();
+        }
+        
+        public override void Load() {
+        }
+        
+        public override void PostLoad() {
+            
+            // plantableDict is private need to find a way of either making it public or another way of making new farmable
+            GardenCatcher.plantableDict.Add(Identifiable.Id.GINGER_VEGGIE,
+                GameContext.Instance.LookupDirector.GetPrefab(Identifiable.Id.GINGER_VEGGIE).gameObject);
+        }
+    }
+}
